a6aeaa993437095db91ae2e7235647d89954a983
TaeVon Lewis <65370736+taevonlewis@users.noreply.github.com>
2023-10-29 01:22:18 -0400

    created signup and login screens w/functionality

--- JerseyTrack/Views/Login.swift
+++ JerseyTrack/Views/Login.swift
@@ -10,36 +10,118 @@ import SwiftUI
 import Firebase
 
 struct Login: View {
-    @State private var email: String = ""
-    @State private var password: String = ""
+    @State private var model: AuthViewModel
+    @FocusState private var focusedField: FocusedField?
+    @State private var isSignupButtonPressed: Bool = false
+    @Binding var showSignup: ActiveView
     
-    var body: some View {
-        VStack(spacing: 20) {
-            TextField("Email", text: $email)
-                .textFieldStyle(.plain)
-                .textInputAutocapitalization(.never)
-            SecureField("Password", text: $password)
-                .textFieldStyle(.plain)
-                .textInputAutocapitalization(.never)
-            Button {
-                login()
-            } label: {
-                Text("Login")
-            }
-
-        }
+    var isSignup: Binding<Bool> {
+        Binding(
+        get: { self.showSignup == .signup },
+        set: { newValue in self.showSignup = newValue ? .signup : .login }
+        )
     }
     
-    private func login() {
-        Auth.auth().signIn(withEmail: email, password: password) { result, error in
-            print("success")
-            if error != nil {
-                print(error!.localizedDescription)
+    public init(model: AuthViewModel, showSignup: Binding<ActiveView>) {
+        self._model = State(initialValue: model)
+        self._showSignup = showSignup
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            NavigationStack {
+                ZStack {
+                    LinearGradient(colors: [.secondaryColor, .primaryColor], startPoint: .topLeading, endPoint: .bottomTrailing)
+                        .ignoresSafeArea()
+                    VStack {
+                        Image("jerseytrack-logo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 300, height: 300)
+                            .padding(.top, 50)
+                        Text("Welcome back!")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .fontDesign(.rounded)
+                            .padding(.bottom, 20)
+                        VStack(spacing: 25) {
+                            JTTextFieldView(placeholder: Text("Email"), isSecure: false, thisField: .email, image: Image(systemName: "envelope"), text: 
+                                    Binding(
+                                        get: { self.model.getEmail() },
+                                        set: { self.model.setEmail($0) }
+                                    )
+                                )
+                                .focused($focusedField, equals: .email)
+                                .overlay(
+                                    VStack {
+                                        Spacer()
+                                        
+                                        if let emailErrorMessage = model.getEmailErrorMessage() {
+                                            ErrorMessageView(message: emailErrorMessage.description)
+                                                .padding(.top, 70)
+                                        }
+                                    }
+                                )
+                            JTTextFieldView(placeholder: Text("Password"), isSecure: true, thisField: .password, image: Image(systemName: "lock"), text:
+                                    Binding(
+                                        get: { self.model.getPassword() },
+                                        set: { self.model.setPassword($0) }
+                                    )
+                                )
+                                .focused($focusedField, equals: .password)
+                                .overlay(
+                                    VStack {
+                                        Spacer()
+                                        
+                                        if let passwordErrorMessage = model.getPasswordErrorMessage() {
+                                            ErrorMessageView(message: passwordErrorMessage.description)
+                                                .padding(.top, 70)
+                                        }
+                                    }
+                                )
+                                .padding(.bottom, 20)
+                            JTGradientButton(title: "Login", action: model.loginUser)
+                                .navigationDestination(isPresented: isSignup, destination: { Signup(model: model, showLogin: $showSignup) })
+                            Button(action: {}, label: {
+                                HStack {
+                                    Text("Already have an account?")
+                                        .foregroundStyle(.black)
+                                    Text("Sign up ")
+                                        .foregroundStyle(.black)
+                                        .fontWeight(.bold)
+                                        .underline()
+                                        .scaleEffect(isSignupButtonPressed ? 0.9 : 1.0)
+                                }
+                                .padding(.bottom, 70)
+                                .onTapGesture {
+                                    showSignup = .signup
+                                }
+                                .onLongPressGesture(minimumDuration: .infinity, maximumDistance: .infinity, pressing: { pressing in
+                                    withAnimation {
+                                        self.isSignupButtonPressed = pressing
+                                    }
+                                }, perform: {})
+                            })
+                            .padding(.top, 120)
+                        }
+                        .padding([.leading, .trailing], geometry.size.width * 0.05)
+                    }
+                }
+                .ignoresSafeArea()
             }
+            .toolbar(.hidden)
         }
     }
 }
 
 #Preview {
-    Login()
+    LoginPreviewWrapper(activeView: .login)
+}
+
+struct LoginPreviewWrapper: View {
+    @State var activeView: ActiveView
+    
+    var body: some View {
+        Login(model: AuthViewModel(), showSignup: $activeView)
+    }
 }
--- JerseyTrack/Views/Signup.swift
+++ JerseyTrack/Views/Signup.swift
@@ -10,35 +10,122 @@ import SwiftUI
 import Firebase
 
 struct Signup: View {
-    @State private var email: String = ""
-    @State private var password: String = ""
+    @State private var model: AuthViewModel
+    @FocusState private var focusedField: FocusedField?
+    @State private var isLoginButtonPressed: Bool = false
+    @Binding var showLogin: ActiveView
     
-    var body: some View {
-        VStack(spacing: 20) {
-            TextField("Email", text: $email)
-                .textFieldStyle(.plain)
-                .textInputAutocapitalization(.never)
-            SecureField("Password", text: $password)
-                .textFieldStyle(.plain)
-                .textInputAutocapitalization(.never)
-            Button {
-                register()
-            } label: {
-                Text("Sign Up")
-            }
-
-        }
+    var isLogin: Binding<Bool> {
+        Binding(
+        get: { self.showLogin == .login },
+        set: { newValue in self.showLogin = newValue ? .login : .signup }
+        )
     }
     
-    private func register() {
-        Auth.auth().createUser(withEmail: email, password: password) { result, error in
-            if error != nil {
-                print(error!.localizedDescription)
+    public init(model: AuthViewModel, showLogin: Binding<ActiveView>) {
+        self._model = State(initialValue: model)
+        self._showLogin = showLogin
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            NavigationStack {
+                ZStack {
+                    LinearGradient(gradient: Gradient(colors: [.primaryColor, .secondaryColor]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                        .ignoresSafeArea()
+                    
+                    VStack {
+                        Image("jerseytrack-logo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 300, height: 300)
+                            .padding(.top, 50)
+                        
+                        Text("Get Started!")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .fontDesign(.rounded)
+                            .padding([.top, .bottom], 20)
+                        
+                        VStack(spacing: 25) {
+                            JTTextFieldView(placeholder: Text("Email"), isSecure: false, thisField: .email, image: Image(systemName: "envelope"), text: 
+                                    Binding(
+                                        get: { self.model.getEmail() },
+                                        set: { self.model.setEmail($0) }
+                                )
+                            )
+                                .focused($focusedField, equals: .email)
+                                .overlay(
+                                    VStack {
+                                        Spacer()
+                                        
+                                        if let emailErrorMessage = model.getEmailErrorMessage() {
+                                            ErrorMessageView(message: emailErrorMessage.description)
+                                                .padding(.top, 70)
+                                        }
+                                    }
+                                )
+                            
+                            JTTextFieldView(placeholder: Text("Password"), isSecure: true, thisField: .password, image: Image(systemName: "lock"), text:
+                                    Binding(
+                                        get: { self.model.getPassword() },
+                                        set: { self.model.setPassword($0) }
+                                )
+                            )
+                                .focused($focusedField, equals: .password)
+                                .overlay(
+                                    VStack {
+                                        Spacer()
+                                        
+                                        if let passwordErrorMessage = model.getPasswordErrorMessage() {
+                                            ErrorMessageView(message: passwordErrorMessage.description)
+                                                .padding(.top, 70)
+                                        }
+                                    }
+                                )
+                                .padding(.bottom, 20)
+                            JTGradientButton(title: "Sign Up", action: model.registerUser)
+                                .navigationDestination(isPresented: isLogin, destination: { Login(model: model, showSignup: $showLogin) })
+                            Button(action: {}, label: {
+                                HStack {
+                                    Text("Already have an account?")
+                                        .foregroundStyle(.black)
+                                    Text("Login")
+                                        .foregroundStyle(.black)
+                                        .fontWeight(.bold)
+                                        .underline()
+                                        .scaleEffect(isLoginButtonPressed ? 0.9 : 1.0)
+                                }
+                                .padding(.bottom, 70)
+                                .onTapGesture {
+                                    showLogin = .login
+                                }
+                                .onLongPressGesture(minimumDuration: .infinity, maximumDistance: .infinity, pressing: { pressing in
+                                    withAnimation {
+                                        self.isLoginButtonPressed = pressing
+                                    }
+                                }, perform: {})
+                            })
+                            .padding(.top, 120)
+                        }
+                        .padding([.leading, .trailing], geometry.size.width * 0.05)
+                    }
+                }
+                .ignoresSafeArea()
             }
+            .toolbar(.hidden)
         }
     }
 }
 
 #Preview {
-    Signup()
+    SignupPreviewWrapper(activeView: .signup)
+}
+
+struct SignupPreviewWrapper: View {
+    @State var activeView: ActiveView
+    
+    var body: some View {
+        Signup(model: AuthViewModel(), showLogin: $activeView)
+    }
 }
